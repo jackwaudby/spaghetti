1645faf132beba6102baf06a380e566f6ffe177b
jackwaudby <j.waudby2@ncl.ac.uk>
2021-01-11 18:24:32 +0000

    logging and cli for client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,93 +2,67 @@
 //! An mpsc channel is used to manage the client connection to the server.
 //! Producers send transaction requests to the consumer which sends the over its TCP connection.
 //! Producers send a oneshot channel with the request in order to receive the response to its request.
-use spaghetti::client::Client;
-use spaghetti::frame::Frame;
-use spaghetti::transaction::{Command, Transaction};
+use spaghetti::client;
 
-use tokio::sync::mpsc::{self, Receiver, Sender};
-use tokio::sync::oneshot;
+use clap::clap_app;
+use config::Config;
+use std::sync::Arc;
 
-use tracing::{info, Level};
+use tracing::{debug, info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> spaghetti::Result<()> {
+    // Read command line arguments using clap macro.
+    let matches = clap_app!( spag =>
+                             (version: "0.1.0")
+                             (author: "J. Waudby <j.waudby2@newcastle.ac.uk>")
+                             (about: "Spaghetti client")
+                             (@arg FILE: -c --config +takes_value "Set a custom config file")
+                             (@arg PORT: -p --port +takes_value "Set port server is listening on")
+                             (@arg ADDRESS: -a --address +takes_value "Set server address")
+                             (@arg LOG: -l --log +takes_value "Set log level")
+
+    )
+    .get_matches();
+
+    // Initialise configuration.
+    // If configuration file is not provided use the default in `Settings.toml`
+    let file = matches.value_of("config").unwrap_or("Settings.toml");
+    let mut settings = Config::default();
+    settings.merge(config::File::with_name(file)).unwrap();
+
+    // For each flag overwrite default with any supplied runtime value.
+    if let Some(p) = matches.value_of("port") {
+        settings.set("port", p).unwrap();
+    }
+
+    if let Some(a) = matches.value_of("address") {
+        settings.set("address", a).unwrap();
+    }
+
+    if let Some(l) = matches.value_of("log") {
+        settings.set("log", l).unwrap();
+    }
+
+    let level = match settings.get_str("log").unwrap().as_str() {
+        "info" => Level::INFO,
+        "debug" => Level::DEBUG,
+        "trace" => Level::TRACE,
+        _ => Level::WARN,
+    };
+
     // All spans/events with a level higher than TRACE written to stdout.
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
+    let subscriber = FmtSubscriber::builder().with_max_level(level).finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     info!("Initialising client");
+    // Wrap configuration in atomic shared reference.
+    // This is ok as configuration never changes at runtime.
+    let config = Arc::new(settings);
 
-    let (tx, mut rx): (Sender<Command>, Receiver<Command>) = mpsc::channel(32);
-
-    // Spawn client resource manager task.
-    let manager = tokio::spawn(async move {
-        // Open a connection to the server.
-        let mut client = Client::connect("127.0.0.1:6142").await.unwrap();
-        // Receive messages from producer.
-        while let Some(cmd) = rx.recv().await {
-            info!("Receive transaction from a producer");
-            // Convert to frame.
-            let frame = cmd.transaction.into_frame();
-            // TODO: this should be a submit function that receives a response.
-            info!("Submit message");
-            let reply = client.submit(&frame).await;
-            info!("Consumer received reply: {:?}", reply);
-            // TODO: Ignore errors and send response back to producer, currently replies with unit struct
-            let _ = cmd.resp.send(Ok(()));
-        }
-    });
-
-    // transaction senders
-    let tx2 = tx.clone();
-
-    let t1 = tokio::spawn(async move {
-        // spawn response channel
-        let (resp_tx, resp_rx) = oneshot::channel();
-
-        // generate transaction
-        let t = Transaction::GetSubscriberData { s_id: 26 };
-        // wrap in command
-        let c = Command {
-            transaction: t,
-            resp: resp_tx,
-        };
-
-        // send transaction to client resource manager
-        tx.send(c).await;
-
-        // await response
-        let res = resp_rx.await;
-        info!("Producer 1 received reply: {:?}", res);
-    });
-
-    let t2 = tokio::spawn(async move {
-        // spawn response channel
-        let (resp_tx, resp_rx) = oneshot::channel();
-
-        // generate transaction
-        let t = Transaction::GetSubscriberData { s_id: 30 };
-        // wrap in command
-        let c = Command {
-            transaction: t,
-            resp: resp_tx,
-        };
-
-        // send transaction to client resource manager
-        tx2.send(c).await;
-
-        // await response
-        let res = resp_rx.await;
-        info!("Producer 2 received reply: {:?}", res);
-    });
-
-    t1.await.unwrap();
-    t2.await.unwrap();
-    manager.await.unwrap();
+    client::run(config).await?;
 
     Ok(())
 }
--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,82 @@
 use crate::connection::Connection;
 use crate::frame::Frame;
+use crate::transaction::{Command, Transaction};
 use crate::Result;
 
+use config::Config;
+use std::sync::Arc;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::sync::oneshot;
+use tracing::{debug, info};
+
+/// Runs the client.
+pub async fn run(conf: Arc<Config>) -> Result<()> {
+    // Mpsc.
+    let mpsc_size = conf.get_int("mpsc_size").unwrap();
+    let (tx, mut rx): (Sender<Command>, Receiver<Command>) = mpsc::channel(mpsc_size as usize);
+
+    // Spawn client resource manager task.
+    let add = conf.get_str("address").unwrap();
+    let port = conf.get_str("port").unwrap();
+    let manager = tokio::spawn(async move {
+        // Open a connection to the server.
+        let mut client = Client::connect(&format!("{}:{}", add, port)[..])
+            .await
+            .unwrap();
+        // Receive messages from producer.
+        while let Some(cmd) = rx.recv().await {
+            debug!("Receive transaction from a producer");
+            // Convert to frame.
+            let frame = cmd.transaction.into_frame();
+            // TODO: this should be a submit function that receives a response.
+            info!("Send message: {:?}", frame);
+            let reply = client.submit(&frame).await;
+            info!("Consumer received reply: {:?}", reply);
+            // TODO: Ignore errors and send response back to producer, currently replies with unit struct
+            let _ = cmd.resp.send(Ok(()));
+        }
+    });
+
+    // Producers create transactions.
+    let prod = conf.get_int("producers").unwrap();
+    let mut handles = vec![];
+
+    for i in 0..prod {
+        // Transmitter end of mpsc.
+        let tx2 = tx.clone();
+
+        let handle = tokio::spawn(async move {
+            // Spawn response channel
+            let (resp_tx, resp_rx) = oneshot::channel();
+
+            // Generate transaction
+            let t = Transaction::GetSubscriberData { s_id: 30 };
+            // wrap in command
+            let c = Command {
+                transaction: t,
+                resp: resp_tx,
+            };
+
+            // Send transaction to client resource manager
+            tx2.send(c).await;
+
+            // Await response
+            let res = resp_rx.await;
+            info!("Producer {:?} received reply: {:?}", i, res);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    manager.await.unwrap();
+
+    Ok(())
+}
 
 pub struct Client {
     /// Tcp connection wrapped with read and write buffers.
