0c051d362650c97d434aaa1da1d242eee2bae0dc
jackwaudby <j.waudby2@ncl.ac.uk>
2021-01-14 17:56:35 +0000

    tatp param gen

--- src/workloads/tatp.rs
+++ src/workloads/tatp.rs
@@ -269,25 +269,62 @@ impl Generator<TatpTransaction> for TatpGenerator {
                 };
                 TatpTransaction::GetNewDestination(payload)
             }
-            _ => {
+            x if x < 0.8 => {
                 // GET_ACCESS_DATA
-                // TODO: should be 0.80
                 let s_id = self.rng.gen_range(1..=self.subscribers);
                 let ai_type = self.rng.gen_range(1..=4);
                 let payload = GetAccessData { s_id, ai_type };
                 TatpTransaction::GetAccessData(payload)
-            } // x if x < 0.82 {
-              //     // UPDATE_SUBSCRIBER_DATA
-              // },
-              // x if x < 0.96 {
-              //     // UPDATE_LOCATION
-              // },
-              // x if x < 0.98 {
-              //       // INSERT_CALL_FORWARDING
-              // }
-              // x if x < 1.0 {
-              //     // DELETE_CALL_FORWARDING
-              // }
+            }
+            x if x < 0.82 => {
+                // UPDATE_SUBSCRIBER_DATA
+                let s_id = self.rng.gen_range(1..=self.subscribers);
+                let sf_type = self.rng.gen_range(1..=4);
+                let bit_1 = self.rng.gen_range(0..=1);
+                let data_a = self.rng.gen_range(0..=255);
+                let payload = UpdateSubscriberData {
+                    s_id,
+                    sf_type,
+                    bit_1,
+                    data_a,
+                };
+                TatpTransaction::UpdateSubscriberData(payload)
+            }
+            x if x < 0.96 => {
+                // UPDATE_LOCATION
+                let s_id = self.rng.gen_range(1..=self.subscribers);
+                let vlr_location = self.rng.gen_range(1..=2 ^ 32 - 1);
+                let payload = UpdateLocationData { s_id, vlr_location };
+                TatpTransaction::UpdateLocationData(payload)
+            }
+            x if x < 0.98 => {
+                // INSERT CALL_FORWARDING
+                let s_id = self.rng.gen_range(1..=self.subscribers);
+                let sf_type = self.rng.gen_range(1..=4);
+                let start_time = helper::get_start_time(&mut self.rng);
+                let end_time = self.rng.gen_range(1..=24);
+                let number_x = helper::get_number_x(&mut self.rng);
+                let payload = InsertCallForwarding {
+                    s_id,
+                    sf_type,
+                    start_time,
+                    end_time,
+                    number_x,
+                };
+                TatpTransaction::InsertCallForwarding(payload)
+            }
+            _ => {
+                // DELETE_CALL_FORWARDING
+                let s_id = self.rng.gen_range(1..=self.subscribers);
+                let sf_type = self.rng.gen_range(1..=4);
+                let start_time = helper::get_start_time(&mut self.rng);
+                let payload = DeleteCallForwarding {
+                    s_id,
+                    sf_type,
+                    start_time,
+                };
+                TatpTransaction::DeleteCallForwarding(payload)
+            }
         };
         Box::new(transaction)
     }
@@ -315,20 +352,45 @@ pub struct GetAccessData {
     ai_type: u8,
 }
 
-// #[derive(Serialize, Deserialize, PartialEq, Debug)]
-// pub struct UpdateSubscriberData {
-//     s_id: u64,
-//     sf_type: u8,
-//     bit_1: u8,
-//     data_a: u
-// }
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct UpdateSubscriberData {
+    s_id: u64,
+    sf_type: u8,
+    bit_1: u8,
+    data_a: u8,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct UpdateLocationData {
+    s_id: u64,
+    vlr_location: u8,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct InsertCallForwarding {
+    s_id: u64,
+    sf_type: u8,
+    start_time: u8,
+    end_time: u8,
+    number_x: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub struct DeleteCallForwarding {
+    s_id: u64,
+    sf_type: u8,
+    start_time: u8,
+}
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub enum TatpTransaction {
     GetSubscriberData(GetSubscriberData),
     GetNewDestination(GetNewDestination),
     GetAccessData(GetAccessData),
-    // UpdateSubscriberData(UpdateSubscriberData),
+    UpdateSubscriberData(UpdateSubscriberData),
+    UpdateLocationData(UpdateLocationData),
+    InsertCallForwarding(InsertCallForwarding),
+    DeleteCallForwarding(DeleteCallForwarding),
 }
 
 impl Transaction for TatpTransaction {
