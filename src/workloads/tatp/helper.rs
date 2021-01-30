8233114b9018548bcc7de20c82a872434ac1431d
jackwaudby <j.waudby2@ncl.ac.uk>
2021-01-30 11:39:04 +0000

    add tatp helpers tests

--- src/workloads/tatp/helper.rs
+++ src/workloads/tatp/helper.rs
@@ -1,27 +1,6 @@
 use rand::rngs::StdRng;
 use rand::Rng;
 
-pub fn to_sub_nbr(s_id: u64) -> String {
-    let mut num = s_id.to_string();
-    for _i in 0..15 {
-        if num.len() == 15 {
-            break;
-        }
-        num = format!("0{}", num);
-    }
-    num
-}
-
-pub fn get_start_time(rng: &mut StdRng) -> u8 {
-    let n: f32 = rng.gen();
-
-    match n {
-        x if x < 0.3333 => 0,
-        x if x < 0.6666 => 8,
-        _ => 16,
-    }
-}
-
 /// Compute the primary key for a `AccessInfo` record.
 pub fn access_info_key(s_id: u64, ai_type: u64) -> u64 {
     (s_id * 10) + ai_type
@@ -44,6 +23,30 @@ pub fn call_forwarding_key(s_id: u64, sf_type: u64, start_time: u64) -> u64 {
     (base * 10) + x
 }
 
+/// Convert subscriber id to `String`.
+pub fn to_sub_nbr(s_id: u64) -> String {
+    let mut num = s_id.to_string();
+    for _i in 0..15 {
+        if num.len() == 15 {
+            break;
+        }
+        num = format!("0{}", num);
+    }
+    num
+}
+
+/// Generate a start time.
+pub fn get_start_time(rng: &mut StdRng) -> u8 {
+    let n: f32 = rng.gen();
+
+    match n {
+        x if x < 0.3333 => 0,
+        x if x < 0.6666 => 8,
+        _ => 16,
+    }
+}
+
+/// Generate active status.
 pub fn is_active(rng: &mut StdRng) -> u64 {
     let f: f32 = rng.gen();
     if f < 0.15 {
@@ -53,6 +56,7 @@ pub fn is_active(rng: &mut StdRng) -> u64 {
     }
 }
 
+/// Get a 15 digit number.
 pub fn get_number_x(rng: &mut StdRng) -> String {
     const CHARSET: &[u8] = b"0123456789";
     const LEN: usize = 15;
@@ -69,7 +73,6 @@ pub fn get_number_x(rng: &mut StdRng) -> String {
 /// Generate random string from upper case A-Z of length `n`.
 pub fn get_data_x(n: usize, rng: &mut StdRng) -> String {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // const LEN: usize = 3;
 
     let data_x: String = (0..n)
         .map(|_| {
@@ -78,4 +81,23 @@ pub fn get_data_x(n: usize, rng: &mut StdRng) -> String {
         })
         .collect();
     data_x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+
+    #[test]
+    fn helpers_test() {
+        assert_eq!(access_info_key(3, 2), 32);
+        assert_eq!(special_facility_key(10, 1, 1), 106);
+        assert_eq!(call_forwarding_key(2, 2, 8), 222);
+        assert_eq!(to_sub_nbr(367), String::from("000000000000367"));
+
+        let mut rng = StdRng::seed_from_u64(42);
+        assert_eq!(get_data_x(3, &mut rng), String::from("NOQ"));
+        assert_eq!(get_number_x(&mut rng), String::from("404781095152050"));
+        assert_eq!(get_start_time(&mut rng), 8);
+    }
 }
