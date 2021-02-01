a9141149e41b0953c195f2e2ffb58e8a49b13478
jackwaudby <j.waudby2@ncl.ac.uk>
2021-02-01 13:54:39 +0000

    add index write op, some tests

--- src/server/storage/index.rs
+++ src/server/storage/index.rs
@@ -1,41 +1,49 @@
-extern crate chashmap;
-
-use std::fmt;
-use std::fmt::Write;
+use std::fmt::{self, Write};
 
 use crate::server::storage::row::Row;
 use chashmap::CHashMap;
-use chashmap::WriteGuard;
 
+/// An `Index` is used to access data.
+///
+/// Each table has at least 1 `Index`, which owns all `Row`s stored in that table.
 #[derive(Debug)]
 pub struct Index {
+    /// Index name.
     name: String,
+    /// Concurrrent hashmap.
     i: CHashMap<u64, Row>,
 }
 
 impl Index {
+    /// Create a new `Index`.
     pub fn init(name: &str) -> Self {
         Index {
             name: String::from(name),
             i: CHashMap::new(),
         }
     }
-    pub fn index_exists(&self, key: u64) -> bool {
+
+    /// Check if a key exists in the index.
+    pub fn key_exists(&self, key: u64) -> bool {
         self.i.contains_key(&key)
     }
+
+    /// Insert a `Row` into the index
     pub fn index_insert(&self, key: u64, row: Row) -> Option<Row> {
-        self.i.insert(key, row)
+        self.i.insert(key, row) // Returns old entry if exists - TODO: map to error.
     }
-    pub fn index_read(&self, key: u64, colums: Vec<&str>) -> String {
-        let read_guard = self.i.get(&key).unwrap(); // None if doesn't exist.
+
+    /// Read `columns` from a `Row` with the given `key`.
+    pub fn index_read(&self, key: u64, columns: Vec<&str>) -> String {
+        let read_guard = self.i.get(&key).unwrap(); // None if doesn't exist - TODO: map to error.
         let row = &*read_guard;
 
         let mut res: String;
 
         res = "[".to_string();
 
-        for column in colums {
-            let value = row.get_value(column).unwrap().unwrap(); // ColumnNotFound, None is no value.
+        for column in columns {
+            let value = row.get_value(column).unwrap().unwrap(); // ColumnNotFound, None is no value - TODO: map to error.
             write!(res, "{}={}, ", column, value).unwrap();
         }
         res.truncate(res.len() - 2);
@@ -43,8 +51,14 @@ impl Index {
         res
     }
 
-    pub fn index_read_mut(&self, key: u64) -> Option<WriteGuard<u64, Row>> {
-        self.i.get_mut(&key)
+    /// Write `values` to `columns`.
+    pub fn index_write(&self, key: u64, columns: Vec<&str>, values: Vec<&str>) -> () {
+        let mut write_guard = self.i.get_mut(&key).unwrap(); //  TODO: map to error.
+        let row = &mut *write_guard;
+
+        for (i, name) in columns.iter().enumerate() {
+            row.set_value(name, values[i]).unwrap();
+        }
     }
 }
 
@@ -97,14 +111,46 @@ mod tests {
     #[test]
     fn index_test() {
         logging(false);
-        let cols = vec!["s_id"];
+
+        assert_eq!(
+            WORKLOAD
+                .get_internals()
+                .get_index("sub_idx")
+                .unwrap()
+                .key_exists(0),
+            true
+        );
+        assert_eq!(
+            format!("{}", WORKLOAD.get_internals().get_index("sub_idx").unwrap()),
+            "[sub_idx,1]"
+        );
+
+        let cols = vec!["bit_4", "byte_2_5"];
         assert_eq!(
             WORKLOAD
                 .get_internals()
                 .get_index("sub_idx")
                 .unwrap()
                 .index_read(0, cols),
-            "[s_id=0]"
+            "[bit_4=1, byte_2_5=205]"
+        );
+
+        let cols = vec!["bit_4", "byte_2_5"];
+        let vals = vec!["0", "69"];
+        WORKLOAD
+            .get_internals()
+            .get_index("sub_idx")
+            .unwrap()
+            .index_write(0, cols, vals);
+
+        let cols = vec!["bit_4", "byte_2_5"];
+        assert_eq!(
+            WORKLOAD
+                .get_internals()
+                .get_index("sub_idx")
+                .unwrap()
+                .index_read(0, cols),
+            "[bit_4=0, byte_2_5=69]"
         );
     }
 }
