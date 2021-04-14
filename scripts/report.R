1bf0bd6e10ef84ca42c10fb4b11b49e87aa54554
jackwaudby <j.waudby2@ncl.ac.uk>
2021-04-14 12:13:03 +0100

    fixes #41

--- scripts/report.R
+++ scripts/report.R
@@ -1,6 +1,7 @@
-fileName = paste0("all-report.pdf")
+today = Sys.Date()
+fileName = paste0(today,"-all-report.pdf")
 Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS/pandoc")
 rmarkdown::render(input = "report.Rmd",
                     output_file = fileName,
-                    output_dir = "../")
+                    output_dir = "../reports/")
 
