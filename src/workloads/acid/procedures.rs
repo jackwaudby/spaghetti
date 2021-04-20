36b941c4f8702b0746a93a85737359cd74e26ceb
jackwaudby <j.waudby2@ncl.ac.uk>
2021-04-20 13:59:50 +0100

    add otv write transaction

--- src/workloads/acid/procedures.rs
+++ src/workloads/acid/procedures.rs
@@ -4,7 +4,7 @@ use crate::server::storage::datatype::{self, Data};
 use crate::workloads::acid::keys::AcidPrimaryKey;
 use crate::workloads::acid::paramgen::{
     G0Read, G0Write, G1aRead, G1aWrite, G1cReadWrite, ImpRead, ImpWrite, LostUpdateRead,
-    LostUpdateWrite,
+    LostUpdateWrite, Otv,
 };
 use crate::workloads::PrimaryKey;
 
@@ -256,6 +256,88 @@ pub fn imp_write(params: ImpWrite, protocol: Arc<Protocol>) -> Result<String, No
     protocol.scheduler.commit(meta.clone())?; // commit
 
     let res = datatype::to_result(None, Some(1), None, None, None).unwrap();
+    Ok(res)
+}
+
+/// Observed Transaction Vanishes (OTV) Write Transaction.
+///
+/// Selects a disjoint person sequence (length 4) and increments `version` property.
+pub fn otv_write(params: Otv, protocol: Arc<Protocol>) -> Result<String, NonFatalError> {
+    let pk1 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p1_id)); // keys
+    let pk2 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p2_id));
+    let pk3 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p3_id));
+    let pk4 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p4_id));
+    let keys = vec![pk1, pk2, pk3, pk4];
+
+    let column: Vec<String> = vec!["version".to_string()]; // column to update
+
+    let meta = protocol.scheduler.register().unwrap(); // register
+
+    // calculate new values -- read current value and increment
+    let calc = |columns: Vec<String>,
+                current: Option<Vec<Data>>,
+                _params: Vec<Data>|
+     -> Result<(Vec<String>, Vec<String>), NonFatalError> {
+        let current_value = match i64::try_from(current.unwrap()[0].clone()) {
+            Ok(value) => value,
+            Err(e) => {
+                protocol.scheduler.abort(meta.clone()).unwrap();
+                return Err(e);
+            }
+        }; // parse to i64 from spaghetti data type
+        let nv = current_value + 1; // increment current value
+        let new_values = vec![nv.to_string()]; // convert to string
+        Ok((columns, new_values)) // new values for columns
+    };
+
+    // TODO: unneeded
+    let params = vec![Data::Int(params.p_id as i64)]; // convert parameters to Vec<Data>
+
+    for pk in keys {
+        protocol.scheduler.update(
+            "person",
+            pk,
+            column.clone(),
+            true,
+            params,
+            &calc,
+            meta.clone(),
+        )?;
+    }
+    protocol.scheduler.commit(meta.clone())?; // commit
+
+    let res = datatype::to_result(None, Some(4), None, None, None).unwrap();
+    Ok(res)
+}
+
+/// Observed Transaction Vanishes (OTV) Read Transaction.
+///
+/// Person nodes are arranged in disjoint sequences of length 4, e.g., (p1, p2, p3, p4).
+/// This transaction reads the `version` property of each person in some disjoint person sequence.
+pub fn otv_read(params: Otv, protocol: Arc<Protocol>) -> Result<String, NonFatalError> {
+    let pk1 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p1_id)); // keys
+    let pk2 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p2_id));
+    let pk3 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p3_id));
+    let pk4 = PrimaryKey::Acid(AcidPrimaryKey::Person(params.p4_id));
+    let keys = vec![pk1, pk2, pk3, pk4];
+
+    let column: Vec<&str> = vec!["version"]; // columns to read
+
+    let meta = protocol.scheduler.register().unwrap(); // register
+
+    let mut reads = vec![];
+
+    for key in keys {
+        let mut read = protocol
+            .scheduler
+            .read("person", key.clone(), &column, meta.clone())?; // read
+        reads.append(&mut read);
+    }
+    protocol.scheduler.commit(meta.clone())?; // commit
+
+    let columns: Vec<&str> = vec!["p1_version", "p2_version", "p3_version", "p4_version"];
+    let res = datatype::to_result(None, None, None, Some(&columns), Some(&reads)).unwrap();
+
     Ok(res)
 }
 
