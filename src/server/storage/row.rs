c288caf044f7ff37b08f3e1ad6a680fd7bc72ca7
jackwaudby <j.waudby2@ncl.ac.uk>
2021-05-01 21:32:46 +0100

    remove self from access history upon committing a write

--- src/server/storage/row.rs
+++ src/server/storage/row.rs
@@ -368,7 +368,7 @@ impl Row {
                     .iter()
                     .position(|a| a == &Access::Write(tid.to_string()))
                     .unwrap();
-                let new_ah = ah.split_off(ind); // remove "old" access information; including self
+                let new_ah = ah.split_off(ind + 1); // remove "old" access information; including self
                 self.access_history = Some(new_ah); // reset
             }
             _ => {}
