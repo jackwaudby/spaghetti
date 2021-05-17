c930ed21b23525f0d665ee4c266ed7061e111eee
jackwaudby <j.waudby2@ncl.ac.uk>
2021-05-18 00:03:18 +0100

    fix single core benchmark

--- benches/bsgt_bench.rs
+++ benches/bsgt_bench.rs
@@ -6,24 +6,20 @@ use std::sync::mpsc;
 use std::sync::Arc;
 
 fn gpc(cores: usize) {
-    let mut c = Config::default();
-    c.merge(config::File::with_name("./tests/Test-basic-sgt.toml"))
+    let mut config = Config::default();
+    config
+        .merge(config::File::with_name("./tests/Test-sgt.toml"))
         .unwrap();
-    let config = Arc::new(c);
-    let workload = helper::init_database(Arc::clone(&config));
-    let scheduler = helper::init_scheduler(Arc::clone(&workload), cores);
-    let (tx, _) = mpsc::channel();
 
-    helper::run(
-        cores,
-        Arc::clone(&scheduler),
-        Arc::clone(&config),
-        tx.clone(),
-    );
+    let workload = helper::init_database(config.clone());
+    let scheduler = helper::init_scheduler(workload, cores);
+    let (tx, _) = mpsc::channel(); // channel to send statistics
+
+    helper::run(cores, scheduler, Arc::new(config), tx);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("basic-sgt");
+    let mut group = c.benchmark_group("sgt");
     group.sample_size(10);
     group.sampling_mode(SamplingMode::Flat);
     group.bench_function("1-core", |b| b.iter(|| gpc(1)));
