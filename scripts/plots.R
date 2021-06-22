8d32b1200791cef78c2b14d8258d7bf2cb239c39
jackwaudby <j.waudby2@ncl.ac.uk>
2021-06-22 13:19:45 +0100

    fix plots

--- scripts/plots.R
+++ scripts/plots.R
@@ -5,10 +5,18 @@ library(dplyr)
 
 waudby <- read_csv(file = './results.csv',col_names = c("sf","protocol","workload","cores","total_time","commits","aborts","errors","total_latency"))
 durner <- read_delim(file = './durner_sgt.csv', delim = ";",col_names = F)
-durner = durner[,c(1,2,3,5,8,9,11,10,18)]
-colnames(durner) <- c("workload","protocol","sf","cores","total_time","commits","aborts","errors","total_latency")
+durner_2pl <- read_delim(file = './durner_2pl.csv', delim = ";",col_names = F)
+durner = durner[,c(1,2,3,5,8,9,11,10,15,18)]
+durner_2pl = durner_2pl[,c(1,2,3,5,8,9,11,10,15,18)]
+colnames(durner) <- c("workload","protocol","sf","cores","total_time","commits","aborts","errors","txn_time","latency")
+colnames(durner_2pl) <- c("workload","protocol","sf","cores","total_time","commits","aborts","errors","txn_time","latency")
+durner$total_latency <- durner$txn_time + durner$latency 
+durner_2pl$total_latency <- durner_2pl$txn_time + durner_2pl$latency 
+durner = durner[,-c(9,10)]
+durner_2pl = durner_2pl[,-c(9,10)]
 durner$protocol = "durner"
-dat = bind_rows(waudby,durner)
+durner_2pl$protocol = "2pl"
+dat = bind_rows(waudby,durner,durner_2pl)
 sf1 = dat %>% filter((sf == 1) | (sf == 100))
 sf3 = dat %>% filter((sf == 3) | (sf == 10000))
 
@@ -47,7 +55,7 @@ ggplot(data=sf3, aes(x=cores, y=aborts/(commits+aborts), group=protocol, colour=
 ggsave("./graphics/smallbank_abort_low.png")
 
 
-# TODO: How do Durner et al. calculate av. latency?
+# Durner: txn time + latency/completed
 ggplot(data=sf1, aes(x=cores, y=total_latency/(commits+aborts+errors), group=protocol, colour=protocol)) +
   geom_line() +
   ylab("av latency (ms)") +
