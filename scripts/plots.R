069923044a908c6c387b4b615cd7cbaa1070660c
jackwaudby <j.waudby2@ncl.ac.uk>
2021-06-22 01:07:03 +0100

    modify plots script

--- scripts/plots.R
+++ scripts/plots.R
@@ -2,10 +2,10 @@ library(ggplot2)
 library(readr)
 library(dplyr)
 
-waudby <- read_csv(file = './results.csv',col_names = c("sf","protocol","workload","cores","total_time","commits","aborts"))
+waudby <- read_csv(file = './results.csv',col_names = c("sf","protocol","workload","cores","total_time","commits","aborts","errors","total_latency"))
 durner <- read_delim(file = './durner_results.csv', delim = ";",col_names = F)
-durner = durner[,c(1,2,3,5,8,9,11)]
-colnames(durner) <- c("workload","protocol","sf","cores","total_time","commits","aborts")
+durner = durner[,c(1,2,3,5,8,9,11,10,18)]
+colnames(durner) <- c("workload","protocol","sf","cores","total_time","commits","aborts","errors","total_latency")
 durner$protocol = "durner"
 dat = bind_rows(waudby,durner)
 sf1 = dat %>% filter((sf == 1) | (sf == 100))
@@ -35,4 +35,16 @@ ggplot(data=sf3, aes(x=cores, y=aborts/(commits+aborts), group=protocol, colour=
   ggtitle("SmallBank - Low Contention (10000 accounts)") +
   theme_bw() 
 
+# TODO: How do Durner et al. calculate av. latency?
+ggplot(data=sf1, aes(x=cores, y=total_latency/(commits+aborts+errors), group=protocol, colour=protocol)) +
+  geom_line() +
+  ylab("av latency (ms)") +
+  ggtitle("SmallBank - High Contention (100 accounts)") +
+  theme_bw() 
+
+ggplot(data=sf3, aes(x=cores, y=total_latency/(commits+aborts+errors), group=protocol, colour=protocol)) +
+  geom_line() +
+  ylab("av latency (ms)") +
+  ggtitle("SmallBank - Low Contention (10000 accounts)") +
+  theme_bw() 
 
