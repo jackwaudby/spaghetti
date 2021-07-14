7d600519f2c0a7669d16de935e0724606741445b
jackwaudby <j.waudby2@ncl.ac.uk>
2021-07-14 18:49:26 +0100

    #83: add call forwarding and special facility loader

--- src/workloads/tatp/loader.rs
+++ src/workloads/tatp/loader.rs
@@ -55,7 +55,7 @@ pub fn populate_tables(
 
         for record in 1..=n_ai {
             let pk = PrimaryKey::Tatp(TatpPrimaryKey::AccessInfo(sid as u64, record as u64));
-            ai.get_mut_exists().insert(pk, ai_offset); // TODO
+            ai.get_mut_exists().insert(pk, ai_offset);
 
             let s_id = Data::Uint(sid as u64);
             ai.get_tuple(0, ai_offset).get().init_value(s_id)?;
@@ -80,124 +80,87 @@ pub fn populate_tables(
     }
     info!("Loaded {} rows into access_info", ai_offset);
 
+    // special facility
+
+    let sf_type_values = vec![1, 2, 3, 4];
+    let start_time_values = vec![0, 8, 16];
+    let mut sf_offset = 0;
+
+    // call forwarding
+
+    let mut cf_offset = 0;
+
+    for sid in 0..population {
+        let n_sf = rng.gen_range(1..=4);
+        let sample = sf_type_values.iter().choose_multiple(rng, n_sf);
+
+        for record in 1..=n_sf {
+            let sf = database.get_mut_table(2);
+            let pk = PrimaryKey::Tatp(TatpPrimaryKey::SpecialFacility(sid as u64, record as u64));
+            sf.get_mut_exists().insert(pk, sf_offset);
+
+            let s_id = Data::Uint(sid as u64);
+            sf.get_tuple(0, sf_offset).get().init_value(s_id)?;
+
+            let sf_type = Data::Uint(*sample[record - 1] as u64);
+            sf.get_tuple(1, sf_offset).get().init_value(sf_type)?;
+
+            let is_active = Data::Uint(tatp::helper::is_active(rng));
+            sf.get_tuple(2, sf_offset).get().init_value(is_active)?;
+
+            let error_cntrl = Data::Uint(rng.gen_range(0..=255));
+            sf.get_tuple(3, sf_offset).get().init_value(error_cntrl)?;
+
+            let data_a = Data::Uint(rng.gen_range(0..=255));
+            sf.get_tuple(4, sf_offset).get().init_value(data_a)?;
+
+            let data_b = Data::VarChar(tatp::helper::get_data_x(5, rng));
+            sf.get_tuple(5, sf_offset).get().init_value(data_b)?;
+
+            drop(sf);
+
+            // for each row, insert [0,3] into call forwarding table
+            let n_cf = rng.gen_range(0..=3);
+            let start_times = start_time_values.iter().choose_multiple(rng, n_cf);
+            if n_cf != 0 {
+                for i in 1..=n_cf {
+                    let cf = database.get_mut_table(3);
+                    let st = *start_times[i - 1];
+                    let pk = PrimaryKey::Tatp(TatpPrimaryKey::CallForwarding(
+                        sid as u64,
+                        record as u64,
+                        st,
+                    ));
+
+                    cf.get_mut_exists().insert(pk, cf_offset);
+                    let s_id = Data::Uint(sid as u64);
+                    cf.get_tuple(0, cf_offset).get().init_value(s_id)?;
+
+                    let sf_type = Data::Uint(*sample[record - 1] as u64);
+                    cf.get_tuple(1, cf_offset).get().init_value(sf_type)?;
+
+                    let start_time = Data::Uint(st);
+                    cf.get_tuple(2, cf_offset).get().init_value(start_time)?;
+
+                    let end_time = Data::Uint(st + rng.gen_range(1..=8));
+                    cf.get_tuple(3, cf_offset).get().init_value(end_time)?;
+
+                    let number_x = Data::VarChar(tatp::helper::get_number_x(rng));
+                    cf.get_tuple(4, cf_offset).get().init_value(number_x)?;
+
+                    drop(cf);
+
+                    cf_offset += 1;
+                }
+            }
+
+            sf_offset += 1;
+        }
+    }
+
+    info!("Loaded {} rows into special_facility", sf_offset);
+
+    info!("Loaded {} rows into call_forwarding", cf_offset);
+
     Ok(())
 }
-
-// /// Populate the `AccessInfo` table.
-// pub fn populate_access_info(
-//     config: Arc<Config>,
-//     tables: &mut HashMap<String, Arc<Table>>,
-//     indexes: &mut HashMap<String, Index>,
-//     rng: &mut StdRng,
-// ) -> Result<()> {
-//     let access_info = tables.get_mut("access_info").unwrap();
-//     let access_idx = indexes.get_mut("access_idx").unwrap();
-
-//     let protocol = config.get_str("protocol")?;
-//     let sf = config.get_int("scale_factor")? as u64;
-//     let subscribers = *TATP_SF_MAP.get(&sf).unwrap();
-
-//     for s_id in 1..=subscribers {
-//         let n_ai = rng.gen_range(1..=4); // generate number of records for a given s_id
-
-//         let sample = ai_type_values.iter().choose_multiple(rng, n_ai); // randomly sample w.o. replacement from range of ai_type values
-//         for record in 1..=n_ai {
-//             let pk = PrimaryKey::Tatp(TatpPrimaryKey::AccessInfo(s_id, record as u64));
-//             let mut row = Row::new(pk.clone(), Arc::clone(&access_info));
-
-//             row.init_value("s_id", Data::from(s_id))?;
-//             row.init_value("ai_type", Data::from(*sample[record - 1] as u64))?;
-//             row.init_value("data_1", Data::from(rng.gen_range(0..=255) as u64))?;
-//             row.init_value("data_2", Data::from(rng.gen_range(0..=255) as u64))?;
-//             row.init_value("data_3", Data::from(helper::get_data_x(3, rng)))?;
-//             row.init_value("data_4", Data::from(helper::get_data_x(5, rng)))?;
-
-//             access_idx.insert(&pk, row);
-//         }
-//     }
-
-//     info!(
-//         "Loaded {} rows into access_info",
-//         access_info.get_num_rows()
-//     );
-
-//     Ok(())
-// }
-
-// /// Populate the `SpecialFacility` table and `CallForwarding` table.
-// pub fn populate_special_facility_call_forwarding(
-//     config: Arc<Config>,
-//     tables: &mut HashMap<String, Arc<Table>>,
-//     indexes: &mut HashMap<String, Index>,
-//     rng: &mut StdRng,
-// ) -> Result<()> {
-//     debug!("Populating special_facility table");
-//     debug!("Populating call_forwarding table");
-
-//     let protocol = config.get_str("protocol")?;
-
-//     let special_facility = tables.get("special_facility").unwrap();
-
-//     let call_forwarding = tables.get("call_forwarding").unwrap();
-
-//     let sf_type_values = vec![1, 2, 3, 4]; // range of values for ai_type records
-//     let start_time_values = vec![0, 8, 16]; // range of values for start_time
-
-//     let sf = config.get_int("scale_factor")? as u64;
-//     let subscribers = *TATP_SF_MAP.get(&sf).unwrap();
-
-//     for s_id in 1..=subscribers {
-//         let n_sf = rng.gen_range(1..=4); // generate number of records for a given s_id
-//         let sample = sf_type_values.iter().choose_multiple(rng, n_sf); // randomly sample w.o. replacement from range of ai_type values
-
-//         for record in 1..=n_sf {
-//             let pk = PrimaryKey::Tatp(TatpPrimaryKey::SpecialFacility(s_id, record as u64)); // calculate primary key
-//             let mut row = Row::new(pk.clone(), Arc::clone(&special_facility)); // initialise empty row
-//             let is_active = helper::is_active(rng); // calculate is_active
-
-//             row.init_value("s_id", Data::from(s_id))?;
-//             row.init_value("sf_type", Data::from(*sample[record - 1] as u64))?;
-//             row.init_value("is_active", Data::from(is_active))?;
-//             row.init_value("error_cntrl", Data::from(rng.gen_range(0..=255) as u64))?;
-//             row.init_value("data_a", Data::from(rng.gen_range(0..=255) as u64))?;
-//             row.init_value("data_b", Data::from(helper::get_data_x(5, rng)))?;
-//             let special_idx = indexes.get_mut("special_idx").unwrap();
-//             special_idx.insert(&pk, row);
-//             drop(special_idx);
-
-//             // for each row, insert [0,3] into call forwarding table
-//             let n_cf = rng.gen_range(0..=3); // generate the number to insert
-//             let start_times = start_time_values.iter().choose_multiple(rng, n_cf); // randomly sample w.o. replacement from range of ai_type values
-//             if n_cf != 0 {
-//                 for i in 1..=n_cf {
-//                     let st = *start_times[i - 1];
-//                     let et = st + rng.gen_range(1..=8);
-//                     let nx = helper::get_number_x(rng);
-
-//                     let pk =
-//                         PrimaryKey::Tatp(TatpPrimaryKey::CallForwarding(s_id, record as u64, st));
-
-//                     let mut row = Row::new(pk.clone(), Arc::clone(&call_forwarding));
-
-//                     row.init_value("s_id", Data::from(s_id))?;
-//                     row.init_value("sf_type", Data::from(*sample[record - 1] as u64))?;
-//                     row.init_value("start_time", Data::from(st))?;
-//                     row.init_value("end_time", Data::from(et))?;
-//                     row.init_value("number_x", Data::from(nx))?;
-//                     let call_idx = indexes.get_mut("call_idx").unwrap();
-//                     call_idx.insert(&pk, row);
-//                     drop(call_idx);
-//                 }
-//             }
-//         }
-//     }
-//     info!(
-//         "Loaded {} rows into special facility",
-//         special_facility.get_num_rows()
-//     );
-//     info!(
-//         "Loaded {} rows into call_forwarding",
-//         call_forwarding.get_num_rows()
-//     );
-//     Ok(())
-// }
