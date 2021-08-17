cf99f7398ead30c3dccf6fdae5af4f7830f1c50e
jackwaudby <j.waudby2@ncl.ac.uk>
2021-08-17 19:34:04 +0100

    improve version display: fix index

--- src/storage/version.rs
+++ src/storage/version.rs
@@ -87,7 +87,7 @@ impl fmt::Display for VersionHistory {
             comma_separated.push_str("version history:");
             comma_separated.push_str("\n");
 
-            for (i, entry) in dat[si - 10..si].iter().rev().enumerate() {
+            for (i, entry) in dat[0..si].iter().rev().enumerate() {
                 comma_separated.push_str(&format!("{}: {}", i, &entry.to_string()));
                 comma_separated.push_str("\n");
             }
