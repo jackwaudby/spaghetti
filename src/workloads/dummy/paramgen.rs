82ae203b41710ac8e19ab993ed81efdf7baf7034
jackwaudby <j.waudby2@ncl.ac.uk>
2022-01-07 21:34:53 +0000

    all write

--- src/workloads/dummy/paramgen.rs
+++ src/workloads/dummy/paramgen.rs
@@ -96,7 +96,7 @@ impl DummyGenerator {
             value,
         };
 
-        if n < 0.5 {
+        if n < 1.0 {
             (
                 DummyTransaction::Write,
                 DummyTransactionProfile::Write(payload),
