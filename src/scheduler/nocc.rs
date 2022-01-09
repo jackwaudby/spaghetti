8fb70b3ff54945e683b51b334dc340e835e2a5d7
jackwaudby <j.waudby2@ncl.ac.uk>
2022-01-09 18:48:24 +0000

    fix tidyup

--- src/scheduler/nocc.rs
+++ src/scheduler/nocc.rs
@@ -122,8 +122,9 @@ impl NoConcurrencyControl {
                 ..
             } = op;
 
-            let table = database.get_table(table_id);
-            let rwtable = table.get_rwtable(offset);
+            let table = database.get_flattable(table_id);
+            let row = table.get_row(offset);
+            let rwtable = row.get_rwtable();
 
             match op_type {
                 OperationType::Read => {
