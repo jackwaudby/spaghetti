edb7e20d4f9269ae9d8cfbaca7e4cd96ba8cdf27
jackwaudby <j.waudby2@ncl.ac.uk>
2022-05-27 15:24:02 +0100

    edit plots

--- msgt_plots.R
+++ msgt_plots.R
@@ -6,6 +6,8 @@ library(patchwork)
 library(scales)
 library(latex2exp)
 
+dir_root = "../msgt/paper/figures/"
+
 file = "./data/22_02_03_ycsb_scalabilty_1.csv"
 file = "./data/22_02_03_ycsb_scalabilty_2.csv"
 file = "./data/22_02_03_ycsb_scalabilty_3.csv"
@@ -30,17 +32,22 @@ col_names = c(
   "serializable_rate"
 )
 raw = read_csv(file = file, col_names = col_names)
+
 raw = raw %>% filter(cores <= 40)
 workload = str_to_title(raw$workload[1])
-
 raw$thpt = raw$commits / (raw$total_time / raw$cores / 1000) / 1000000
 raw$abr = raw$external / (raw$commits + raw$external+ raw$internal)
 raw$lat = raw$total_latency / (raw$commits + raw$external + raw$internal)
-
 raw = raw %>% 
   mutate(protocol = str_replace(protocol, "msgt-std", "msgt"))
+raw$abr = raw$abr * 100
 
 #### SCALABILITY ####
+
+msgt = raw[raw$protocol == "msgt",]
+sgt = raw[raw$protocol == "sgt",]
+((msgt$thpt / sgt$thpt ) - 1)*100
+(sgt$lat/msgt$lat)
 
 sf1 = raw %>%
   group_by(protocol, cores) %>%
@@ -48,169 +55,114 @@ sf1 = raw %>%
             abr = median(abr),
             lat = median(lat))
 
-p1 = ggplot(data = sf1, aes(
-  x = cores,
-  y = thpt,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  ylab("thpt (million/s)") +
-  theme_bw()
 
-p2 = ggplot(data = sf1, aes(
-  x = cores,
-  y = abr,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  ylab("abort rate") +
-  theme_bw()
 
-p3 = ggplot(data = sf1, aes(
-  x = cores,
-  y = lat,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  ylab("av latency (ms)") +
-  theme_bw() +
-  scale_y_log10()
+(s1 = ggplot(data = sf1, aes(x = cores,y = thpt,group = protocol,colour = protocol)) + 
+  geom_line() + ylab("thpt (million/s)") + labs(color="") + theme_bw() + 
+    theme(legend.position="top",text = element_text(size = 18)))
 
-combined <- p1 + p2 + p3 & theme(legend.position = "top",
-                                 text = element_text(size = 20)
-)
-combined + plot_layout(guides = "collect")
-ggsave(paste0("../msgt/paper/figures/ycsb_scalability.pdf"),width = 18, height = 6,device = "pdf")
+(s2 = ggplot(data = sf1, aes(x = cores,y = abr,group = protocol,colour = protocol)) +
+  geom_line() + ylab("abort rate") + labs(color="") + theme_bw() + 
+    theme(legend.position="top",text = element_text(size = 18)))
+
+(s3 = ggplot(data = sf1, aes(x = cores,y = lat,group = protocol,colour = protocol)) +
+  geom_line() + ylab("av latency (ms)") + labs(color="")+ theme_bw() + scale_y_log10() + 
+    theme(legend.position="top",text = element_text(size = 18)))
+
+combined <- s1 + s2 + s3 & theme(legend.position = "top",text = element_text(size = 20))
+(sAll = combined + plot_layout(guides = "collect"))
+
+s_root = "ycsb_scalability"
+ggsave(paste0(dir_root,s_root,"_thpt.pdf"), s1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,s_root,"_abr.pdf"), s2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,s_root,"_lat.pdf"), s3, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,".pdf"),sAll,width = 18, height = 6,device = "pdf")
 
 #### ISOLATION ####
 
-p1 = ggplot(data = raw, aes(
-  x = serializable_rate,
-  y = thpt,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Serializable Transactions ($\\omega$)')) +
-  ylab("thpt (million/s)") +
-  theme_bw()
+raw = raw %>% filter(serializable_rate <= 0.8)
 
-p2 = ggplot(data = raw, aes(
-  x = serializable_rate,
-  y = abr,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Serializable Transactions ($\\omega$)')) +
-  ylab("abort rate") +
-  theme_bw()
+msgt = raw[raw$protocol == "msgt",]
+sgt = raw[raw$protocol == "sgt",]
 
-p3 = ggplot(data = raw, aes(
-  x = serializable_rate,
-  y = lat,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Serializable Transactions ($\\omega$)')) +
-  ylab("av latency (ms)") +
-  theme_bw() +
-  scale_y_log10()
+(msgt$thpt)/sgt$thpt
 
-combined <- p1 + p2 + p3 & theme(legend.position = "top",
-                                 text = element_text(size = 20)
-)
+sgt$abr/(msgt$abr)
+
+(p1 = ggplot(data = raw, aes(x = serializable_rate, y = thpt, group = protocol, colour = protocol)) +
+  geom_line() + xlab(TeX('% of Serializable Transactions ($\\omega$)')) + ylab("thpt (million/s)") + 
+    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
+
+(p2 = ggplot(data = raw, aes(x = serializable_rate, y = abr, group = protocol, colour = protocol)) +
+  geom_line() + xlab(TeX('% of Serializable Transactions ($\\omega$)')) + ylab("abort rate") +
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
+
+(p3 = ggplot(data = raw, aes(x = serializable_rate, y = lat, group = protocol, colour = protocol)) +
+  geom_line() + xlab(TeX('% of Serializable Transactions ($\\omega$)')) + ylab("av latency (ms)") + scale_y_log10() +
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
+
+combined <- p1 + p2 + p3 & theme(legend.position = "top", text = element_text(size = 20))
 combined + plot_layout(guides = "collect")
-ggsave(paste0("../msgt/paper/figures/ycsb_isolation.pdf"),width = 18, height = 6,device = "pdf")
 
-raw
+
+file_root = "ycsb_isolation"
+
+ggsave(paste0(dir_root,file_root,"_thpt.pdf"), p1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,file_root,"_abr.pdf"), p2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,file_root,"_lat.pdf"), p3, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,".pdf"),width = 18, height = 6,device = "pdf")
 
 #### UPDATE ####
 
-p1 = ggplot(data = raw, aes(
-  x = update_rate,
-  y = thpt,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Update Transactions ($U$)')) +
-  ylab("thpt (million/s)") +
-  theme_bw()
+(u1 = ggplot(data = raw, aes(x = update_rate,y = thpt,group = protocol,colour = protocol)) +
+  geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("thpt (million/s)") + 
+   labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
 
-p2 = ggplot(data = raw, aes(
-  x = update_rate,
-  y = abr,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Update Transactions ($U$)')) +
-  ylab("abort rate") +
-  theme_bw()
+(u2 = ggplot(data = raw, aes(x = update_rate,y = abr,group = protocol,colour = protocol)) +
+  geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("abort rate") + 
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
 
-p3 = ggplot(data = raw, aes(
-  x = update_rate,
-  y = lat,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('% of Update Transactions ($U$)')) +
-  ylab("av latency (ms)") +
-  theme_bw() +
-  scale_y_log10()
+(u3 = ggplot(data = raw, aes(x = update_rate,y = lat,group = protocol,colour = protocol)) +
+  geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("av latency (ms)") + theme_bw() + scale_y_log10() + 
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
 
-combined <- p1 + p2 + p3 & theme(legend.position = "top",
-                                 text = element_text(size = 20)
-)
-combined + plot_layout(guides = "collect")
-ggsave(paste0("../msgt/paper/figures/ycsb_update.pdf"),width = 18, height = 6,device = "pdf")
+combined <- u1 + u2 + u3 & theme(legend.position = "top",text = element_text(size = 20))
+uAll = combined + plot_layout(guides = "collect")
+
+ur_root = "ycsb_update_rate"
+ggsave(paste0(dir_root,ur_root,"_thpt.pdf"), u1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,ur_root,"_abr.pdf"), u2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,ur_root,"_lat.pdf"), u3, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,".pdf"),uAll,width = 18, height = 6,device = "pdf")
 
 
 #### CONTENTION ####
 
-p1 = ggplot(data = raw, aes(
-  x = theta,
-  y = thpt,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  ylab("thpt (million/s)") +
-  xlab(TeX('Skew Factor ($\\theta$)')) +
-  theme_bw()
+msgt = raw[raw$protocol == "msgt",]
+sgt = raw[raw$protocol == "sgt",]
+((msgt$thpt / sgt$thpt ) - 1)*100
 
-p2 = ggplot(data = raw, aes(
-  x = theta,
-  y = abr,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('Skew Factor ($\\theta$)')) +
-  ylab("abort rate") +
-  theme_bw()
+sgt$abr/msgt$abr
+sgt$abr - msgt$abr
+(1-msgt$lat/sgt$lat)*100
 
-p3 = ggplot(data = raw, aes(
-  x = theta,
-  y = lat,
-  group = protocol,
-  colour = protocol
-)) +
-  geom_line() +
-  xlab(TeX('Skew Factor ($\\theta$)')) +
-  ylab("av latency (ms)") +
-  theme_bw() +
-  scale_y_log10()
+(c1 = ggplot(data = raw, aes(x = theta,y = thpt,group = protocol,colour = protocol)) +
+  geom_line() + ylab("thpt (million/s)") + xlab(TeX('Skew Factor ($\\theta$)')) +
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
 
-combined <- p1 + p2 + p3& theme(legend.position = "top",
-                                 text = element_text(size = 20)
-)
-combined + plot_layout(guides = "collect")
-ggsave(paste0("../msgt/paper/ycsb_contention.pdf"),width = 18, height = 6,device = "pdf")
+(c2 = ggplot(data = raw, aes(x = theta,y = abr,group = protocol,colour = protocol)) +
+  geom_line() + xlab(TeX('Skew Factor ($\\theta$)')) + ylab("abort rate") +
+  labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
 
+(c3 = ggplot(data = raw, aes(x = theta,y = lat,group = protocol,colour = protocol)) +
+  geom_line() + xlab(TeX('Skew Factor ($\\theta$)')) + ylab("av latency (ms)") + scale_y_log10() +
+    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)))
+
+combined <- c1 + c2 + c3 & theme(legend.position = "top", text = element_text(size = 20))
+cAll = combined + plot_layout(guides = "collect")
+
+c_root = "ycsb_contention"
+ggsave(paste0(dir_root,c_root,"_thpt.pdf"), c1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,c_root,"_abr.pdf"), c2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,c_root,"_lat.pdf"), c3, width = 6, height = 4,device = "pdf")
+ggsave(paste0(dir_root,".pdf"),cAll,width = 18, height = 6,device = "pdf")
