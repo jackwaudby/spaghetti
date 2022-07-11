548878315eb3ff5d506b42ca6667e0260bbccf50
Jack Waudby <jackwaudby@ghost.lan>
2022-07-11 20:49:29 +0100

    no panic

--- src/scheduler/msgt.rs
+++ src/scheduler/msgt.rs
@@ -149,12 +149,12 @@ impl MixedSerializationGraph {
                 // }
 
                 // need to do another check
-                if self.relevant_cycle_check && (attempts % 100 == 0) {
-                    let is_cycle = self.cycle_check_init(this_ref); // cycle check
-                    if is_cycle {
-                        return false;
-                    }
-                }
+                // if self.relevant_cycle_check && (attempts % 100 == 0) {
+                //     let is_cycle = self.cycle_check_init(this_ref); // cycle check
+                //     if is_cycle {
+                //         return false;
+                //     }
+                // }
 
                 return true;
             };
@@ -241,11 +241,11 @@ impl MixedSerializationGraph {
             let incoming = cur.get_incoming();
             for edge in incoming {
                 // if the edge is not relevant then ignore!
-                if self.relevant_cycle_check {
-                    if self.is_edge_relevant(root_lvl, &edge) {
-                        continue;
-                    }
-                }
+                // if self.relevant_cycle_check {
+                //     if self.is_edge_relevant(root_lvl, &edge) {
+                //         continue;
+                //     }
+                // }
 
                 let id = edge.extract_id() as usize;
                 // if visit_path.contains(&id) {
@@ -651,12 +651,12 @@ impl MixedSerializationGraph {
             //         }
             //     }
 
-            if self.relevant_cycle_check && (attempts % 10000 == 0) {
-                let is_cycle = self.cycle_check_init(this_node);
-                if is_cycle {
-                    this_node.set_aborted();
-                }
-            }
+            // if self.relevant_cycle_check && (attempts % 10000 == 0) {
+            //     let is_cycle = self.cycle_check_init(this_node);
+            //     if is_cycle {
+            //         this_node.set_aborted();
+            //     }
+            // }
             // }
 
             attempts += 1;
