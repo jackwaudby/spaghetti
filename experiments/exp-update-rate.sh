446866bf612d59698f248561f1c7689f43d3f21b
Jack Waudby <jackwaudby@ghost.lan>
2022-07-19 13:48:41 +0100

    fix update rate contention level

--- experiments/exp-update-rate.sh
+++ experiments/exp-update-rate.sh
@@ -2,7 +2,7 @@
 
 # Update Rate Experiment
 # cores = 40
-# U = 0.0-1.0, omega = 0.5, theta = 0.6, queries = 10
+# U = 0.0-1.0, omega = 0.2, theta = 0.8, queries = 10
 
 cd ../
 
@@ -13,7 +13,7 @@ cargo build --release
 
 for protocol in msgt sgt; do
     for ur in 0.0 0.2 0.4 0.6 0.8 1.0; do
-        ./target/release/spag -p $protocol -s 2 -c 40 -t $1 -w ycsb -u $ur -i 0.2 -h 0.6 -d reduced -q 10
+        ./target/release/spag -p $protocol -s 2 -c 40 -t $1 -w ycsb -u $ur -i 0.2 -h 0.8 -d reduced -q 10
         sleep 5
     done
 done
--- tpctc/tpctc_plots.R
+++ tpctc/tpctc_plots.R
@@ -136,6 +136,7 @@ scal_file_root = "ycsb_scalability"
 # percentage increase = ((new - old)/old)*100
 ((df$thpt[1:5] - df$thpt[6:10])/ df$thpt[6:10]) * 100
 
+# Throughput
 (s1 = ggplot(data = df, aes(x = cores,y = thpt,group = protocol,colour = protocol)) + 
    geom_line() + ylab("thpt (million/s)") + labs(color="") + theme_bw() + 
    theme(legend.position="top",text = element_text(size = 18)) +
@@ -163,4 +164,35 @@ ggsave(paste0(scal_file_root,"_abr.png"), s2, width = 6, height = 4,device = "pn
 
 ggsave(paste0(scal_file_root,"_lat.pdf"), s3, width = 6, height = 4,device = "pdf")
 ggsave(paste0(scal_file_root,"_lat.png"), s3, width = 6, height = 4,device = "png")
+
+#### Update Rate Experiment ####
+dat_file = "./exp-update-rate-results.csv"
+df = read_csv(file = dat_file, col_names = col_names)
+df = renameProtocols(df) 
+df = computeMetrics(df)
+ur_file_root = "ycsb_update_rate"
+
+(u1 = ggplot(data = raw, aes(x = update_rate,y = thpt,group = protocol,colour = protocol)) +
+    geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("thpt (million/s)") + 
+    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)) +
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+# Abort rate 
+(u2 = ggplot(data = raw, aes(x = update_rate,y = abr,group = protocol,colour = protocol)) +
+    geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("abort rate") + 
+    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)) +
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+# Latency
+(u3 = ggplot(data = raw, aes(x = update_rate,y = lat,group = protocol,colour = protocol)) +
+    geom_line() + xlab(TeX('% of Update Transactions ($U$)')) + ylab("av latency (ms)") + theme_bw() + scale_y_log10() + 
+    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)) +
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+ggsave(paste0(ur_file_root,"_thpt.pdf"), u1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(ur_file_root,"_thpt.pdf"), u2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(ur_file_root,"_thpt.pdf"), u3, width = 6, height = 4,device = "pdf")
+
+
+
 
