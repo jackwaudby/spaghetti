362fe362f18a7f78db4868ceb2bdf2848dc94d1c
Jack Waudby <jackwaudby@ghost.lan>
2022-07-19 01:34:35 +0100

    fix contention exp sf

--- experiments/exp-contention.sh
+++ experiments/exp-contention.sh
@@ -13,7 +13,7 @@ cargo build --release
 
 for protocol in msgt sgt; do
     for con in 0.6 0.7 0.75 0.8 0.85 0.9; do
-        ./target/release/spag -p $protocol -s 1 -c 40 -t $1 -w ycsb -u 0.5 -i 0.2 -h $con -d reduced -q 10
+        ./target/release/spag -p $protocol -s 2 -c 40 -t $1 -w ycsb -u 0.5 -i 0.2 -h $con -d reduced -q 10
         sleep 5
     done
 done
