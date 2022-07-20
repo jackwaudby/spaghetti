77323a3b8bc46e2dcb3d28b220e23e255194755e
Jack Waudby <jackwaudby@ghost.lan>
2022-07-20 19:47:47 +0100

    tidy up vldb plots

--- vldb/vldb_plots.R
+++ vldb/vldb_plots.R
@@ -50,92 +50,6 @@ computeMetrics <- function(df) {
   return(df)
 }
 
-#### Overhead Experiment ####
-file = "../results/exp-overhead-results.csv"
-df = read_csv(file = file, col_names = col_names)
-df = renameProtocols(df) 
-df = computeMetrics(df)
-
-for (w in c("smallbank","ycsb","tatp")) {
-  temp = df[which(df$workload == w),]
-  cat(paste0("workload: ",w,"\n"))
-  
-  (noccThpt = temp[which(temp$protocol == "nocc"),]$thpt)
-  (sgtThpt = temp[which(temp$protocol == "sgt"),]$thpt)
-  (msgtThpt = temp[which(temp$protocol == "msgt-red"),]$thpt)
-  (sgtOverhead = ((noccThpt - sgtThpt) / noccThpt)*100)
-  (msgtOverhead = ((noccThpt - msgtThpt) / noccThpt)*100)
-  
-  cat(paste0("nocc thpt: ",round(noccThpt, 2),"M\n"))
-  cat(paste0("sgt thpt: ",round(sgtThpt, 2),"M\n"))
-  cat(paste0("msgt thpt: ",round(msgtThpt, 2),"M\n"))
-  cat(paste0("sgt overhead: ",round(sgtOverhead, 2),"%\n"))
-  cat(paste0("msgt overhead: ",round(msgtOverhead, 2),"%\n"))
-  cat("\n")
-}
-
-(noccThpt = df[which(df$protocol == "nocc"),]$thpt)
-(sgtThpt = df[which(df$protocol == "sgt"),]$thpt)
-(msgtThpt = df[which(df$protocol == "msgt-red"),]$thpt)
-(sgtOverhead = ((noccThpt - sgtThpt) / noccThpt)*100)
-(msgtOverhead = ((noccThpt - msgtThpt) / noccThpt)*100)
-
-
-#### Isolation Experiment ####
-file = "../results/exp-isolation-results.csv"
-df = read_csv(file = file, col_names = col_names)
-df = renameProtocolsTPCTC(df) 
-df = computeMetrics(df)
-
-(p1 = ggplot(data = df, aes(x = serializable_rate, y = thpt, group = protocol, colour = protocol)) +
-    geom_line() + xlab(TeX('% of Serializable Transactions ($\\omega$)')) + ylab("thpt (million/s)") + 
-    labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)) + 
-    scale_color_manual(values=c("#CC6666", "#055099")))
-
-ggplot(data = df, 
-       aes(x = serializable_rate, 
-           y = abr, 
-           group = protocol, 
-           colour = protocol)) +
-  geom_line()
-
-ggplot(data = df, 
-       aes(x = serializable_rate, 
-           y = lat, 
-           group = protocol, 
-           colour = protocol)) +
-  geom_line()
-
-
-ggplot(data = df, 
-       aes(x = serializable_rate, 
-           y = com, 
-           group = protocol, 
-           colour = protocol)) +
-        geom_line()
-
-msgt = df[1:6,]
-total = msgt$g0 + msgt$g1 + msgt$g2
-
-msgt$g0 = (msgt$g0 / total) * 100
-msgt$g1 = (msgt$g1 / total) * 100
-msgt$g2 = (msgt$g2 / total) * 100
-
-ggplot(data = msgt, 
-       aes(x = serializable_rate)) +
-  geom_line(aes(y = g0)) +
-  geom_line(aes(y = g1)) +
-  geom_line(aes(y = g2))
-
-
-
-ggplot(data = df[1:6,], 
-       aes(x = serializable_rate, 
-           y = apl)) +
-  geom_line()
-
-
-
 #### SCALABILITY ####
 dat_file = "./data/exp-scalability-results.csv"
 df = read_csv(file = dat_file, col_names = col_names)
@@ -158,14 +72,14 @@ scal_file_root = "./graphics/ycsb_scalability"
 combined <- s1 + s2 & theme(legend.position = "top",text = element_text(size = 20))
 (sAll = combined + plot_layout(guides = "collect"))
 
-ggsave(paste0(file_root,".pdf"),sAll,width = 18, height = 6,device = "pdf")
+ggsave(paste0(scal_file_root,".pdf"),sAll,width = 18, height = 6,device = "pdf")
 
 #### ISOLATION ####
 dat_file = "./data/exp-isolation-results.csv"
 df = read_csv(file = dat_file, col_names = col_names)
 df = renameProtocols(df) 
 df = computeMetrics(df)
-file_root = "./graphics/ycsb_isolation"
+iso_file_root = "./graphics/ycsb_isolation"
 
 # Throughput 
 (p1 = ggplot(data = df, aes(x = serializable_rate, y = thpt, group = protocol, colour = protocol)) +
@@ -197,7 +111,7 @@ file_root = "./graphics/ycsb_isolation"
 combined <- p1 + p2 + p3 & theme(legend.position = "top", text = element_text(size = 20))
 (combined + plot_layout(guides = "collect"))
 
-ggsave(paste0(file_root,".pdf"),width = 18, height = 6,device = "pdf")
+ggsave(paste0(iso_file_root,".pdf"),width = 18, height = 6,device = "pdf")
 
 #### UPDATE ####
 dat_file = "./data/exp-update-rate-results.csv"
@@ -212,7 +126,7 @@ ur_file_root = "./graphics/ycsb_update_rate"
     labs(color="") + theme_bw() + theme(legend.position="top",text = element_text(size = 18)) +
     scale_color_manual(values=c("#CC6666", "#055099")))
 
-ggsave(paste0(ur_file_root,"_thpt.pdf"), u1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(ur_file_root,".pdf"), u1, width = 6, height = 4,device = "pdf")
 
 #### CONTENTION ####
 dat_file = "./data/exp-contention-results.csv"
