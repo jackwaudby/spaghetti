4f1cef1222be2df538690d57884383e9a1165925
Jack Waudby <jackwaudby@ghost.lan>
2022-07-21 20:20:18 +0100

    phd workshop plots

--- phd-workshop/phd_workshop_plots.R
+++ phd-workshop/phd_workshop_plots.R
@@ -35,3 +35,44 @@ col_names = c("sf","protocol","workload","cores",
               "queries","dfs","runtime","commits","aborts",
               "not_found","txn_time","commit_time","wait_time",
               "latency","rw","wr","rw","g0","g1","g2","path")
+
+#### Isolation Experiment ####
+dat_file = "./data/exp-isolation-results-sf2.csv"
+df = read_csv(file = dat_file, col_names = col_names)
+df = renameProtocols(df) 
+df = computeMetrics(df)
+file_root = "./graphics/ycsb_isolation"
+
+# Metrics 
+((df[1:6,]$thpt - df[7:12,]$thpt) / df[7:12,]$thpt) * 100
+
+# Throughput 
+(p1 = ggplot(data = df, aes(x = serializable_rate, y = thpt, group = protocol, colour = protocol)) +
+    geom_line() + 
+    xlab(TeX('% of Serializable Transactions ($\\omega$)')) + 
+    ylab("thpt (million/s)") + 
+    labs(color="") + 
+    theme_bw() + theme(legend.position="top",text = element_text(size = 18)) + 
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+# Abort rate 
+(p2 = ggplot(data = df, aes(x = serializable_rate, y = abr, group = protocol, colour = protocol)) +
+    geom_line() + 
+    xlab(TeX('% of Serializable Transactions ($\\omega$)')) + 
+    ylab("abort rate (%)") +
+    labs(color="") + 
+    theme_bw() + theme(legend.position="top",text = element_text(size = 18)) + 
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+# Latency
+(p3 = ggplot(data = df, aes(x = serializable_rate, y = lat, group = protocol, colour = protocol)) +
+    geom_line() + 
+    xlab(TeX('% of Serializable Transactions ($\\omega$)')) + 
+    ylab("av latency (ms)") + 
+    labs(color="") + 
+    theme_bw() + theme(legend.position="top",text = element_text(size = 18)) +
+    scale_color_manual(values=c("#CC6666", "#055099")))
+
+ggsave(paste0(file_root,"_thpt.pdf"), p1, width = 6, height = 4,device = "pdf")
+ggsave(paste0(file_root,"_abr.pdf"), p2, width = 6, height = 4,device = "pdf")
+ggsave(paste0(file_root,"_lat.pdf"), p3, width = 6, height = 4,device = "pdf")
