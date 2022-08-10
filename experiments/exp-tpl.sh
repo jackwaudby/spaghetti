1fcedd0d570b70da356120b45719ae426e7b4027
Jack Waudby <jackwaudby@ghost.lan>
2022-08-10 14:46:57 +0100

    update 2pl exp

--- experiments/exp-tpl.sh
+++ experiments/exp-tpl.sh
@@ -5,11 +5,22 @@ rm exp-tpl-results.csv
 
 cargo build --release
 
-for scalefactor in 1 2 3; do
-    for cores in 1 5 10 20 30 40; do
-        ./target/release/spag --protocol tpl --scalefactor $scalefactor --cores $cores --workload smallbank --transactions $1
-        sleep 5
-    done
+# smallbank
+for cores in 1 5 10 20 30 40; do
+    ./target/release/spag --protocol tpl --scalefactor 1 --cores $cores --workload smallbank --transactions $1
+    sleep 5
+done
+
+# tatp
+for cores in 1 5 10 20 30 40; do
+    ./target/release/spag --protocol tpl --scalefactor 1 --cores $cores --workload tatp --transactions $1
+    sleep 5
+done
+
+# ycsb
+for cores in 1 5 10 20 30 40; do
+    ./target/release/spag --protocol tpl --scalefactor 1 --cores $cores --workload ycsb --transactions $1 --theta 0.8
+    sleep 5
 done
 
 mv results.csv results/exp-tpl-results.csv
